22622fdddd17200180611ba78d9f9b9b92bb6324
52HUB Local Maintainer <52hub-local@example.invalid>
2026-06-04 16:06:36 +0800

    Use 5x4 product card images

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -30,7 +30,7 @@
           <div v-if="loading" class="grid grid-cols-2 gap-3 md:gap-4 md:grid-cols-3 lg:grid-cols-4">
             <div v-for="i in 6" :key="i"
               class="theme-panel rounded-2xl border overflow-hidden flex flex-col">
-              <div class="h-36 md:h-56 theme-skeleton"></div>
+              <div class="aspect-[5/4] theme-skeleton"></div>
               <div class="p-3 md:p-5 space-y-3">
                 <div class="h-3 w-16 rounded theme-skeleton"></div>
                 <div class="h-5 w-3/4 rounded theme-skeleton"></div>
